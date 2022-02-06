e0e250783c6f40f7ef0de580e151ed70578649f3
Vinayak56 <vinayakunde962001@gmail.com>
2022-02-06 20:49:26 +0530

    Initial commit

---
+++ wiki_extractor.py
@@ -0,0 +1,33 @@
+import wikipedia
+from scrapy import Selector
+import requests
+import html2text
+import json
+class wiki:
+    def __init__(self,keyword,num_urls,output):
+        self.__keyword = keyword
+        self.__num_urls = num_urls
+        self.__output = output
+    def outList(self):
+        link_list = []
+        key_list = wikipedia.search(self.__keyword,results=self.__num_urls)
+        h = html2text.HTML2Text()
+        h.ignore_links = True
+        h.bypass_tables = False
+        xpat = "/html/body/div[3]/div[3]/div[5]/div[1]/p[2]"
+        for i in key_list:
+            page = wikipedia.page(i)
+            url = page.url
+            html = requests.get(url).content
+            sel = Selector(text=html)
+            p = sel.xpath(xpat).extract()
+            para = h.handle(p[0]).replace("\n"," ").replace("*","")
+            link_list.append({"url":url,"paragraph":para})
+        with open(self.__output, "w") as final:
+            json.dump(link_list, final)
+            
+__keyword = input("Enter keyword : ")
+__num_urls = int(input("Enter number of urls : "))
+__output = input("Enter name of json file : ")
+obj = wiki(__keyword,__num_urls,__output)
+obj.outList()
